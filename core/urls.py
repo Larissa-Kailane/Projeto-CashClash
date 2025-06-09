d72dadf8b9b3dff3c2bbc30bba41c31167d8107c
Larissa-Kailane <larissakailanelv@gmail.com>
2025-06-09 12:55:33 -0300

    Subindo outras telas (falta adicionar perguntas/testando a navegação)

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.telaInicial, name="telaInicial"),
+    path("", views.tela_inicial, name="telaInicial"),
     path("iniciar/", views.iniciar_jogo, name="iniciar_jogo"),
-    path("pergunta/", views.telaPerguntas, name="telaPerguntas"),
+    path("pergunta/", views.tela_pergunta, name="telaPerguntas"),
     path("feedback/", views.tela_feedback, name="tela_feedback"),
 ]
